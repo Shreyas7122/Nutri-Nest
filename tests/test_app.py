9e30c591986ffcd54977bc8c14633d364dacb216
Shreyas Dixit <122428959+Shreyas7122@users.noreply.github.com>
2024-08-20 18:30:13 +0530

    automation testing menu page

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,8 @@ def test_login_page():
     tester = app.test_client()
     response = tester.get('/about')
     assert response.status_code == 200
+    
+def test_login_page():
+    tester = app.test_client()
+    response = tester.get('/menu')
+    assert response.status_code == 200
