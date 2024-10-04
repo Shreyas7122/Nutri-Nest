d1a3606704d6889060136f3794cdca7382ff81a3
Shreyas Dixit <122428959+Shreyas7122@users.noreply.github.com>
2024-10-04 19:32:58 +0530

    change

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 mysql = MySQL(app)
-# change
 @app.route('/')
 def index():
     return render_template('index.html')
