1a2c95c67fb77e2eb14db928afbc0deed3b897e6
Shreyas Dixit <122428959+Shreyas7122@users.noreply.github.com>
2024-10-30 15:49:22 +0530

    added bin

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
-#m
 mysql = MySQL(app)
 @app.route('/')
 def index():
