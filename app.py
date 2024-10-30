5f77bd30fea612e06d582ff79c7a4ef0cc3a7a31
Shreyas Dixit <122428959+Shreyas7122@users.noreply.github.com>
2024-10-30 15:44:50 +0530

    smal change

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
+#m
 mysql = MySQL(app)
 @app.route('/')
 def index():
